99ee41bd30b3cf1c16981c70e7e5950e7a1917c0
neeraj <neerajpc799@gmail.com>
2026-02-05 16:58:02 +0530

    Changes in homepage

--- frontend/src/pages/AuthCallbackpage.tsx
+++ frontend/src/pages/AuthCallbackpage.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useCreateUser } from "../api/UserApi";
 import { useNavigate } from "react-router-dom";
 
